43f18324ea61c94dfe1fab2c82a1972173ce4dfb
Stael Edson <edsonstael@gmail.com>
2023-02-03 11:38:28 -0300

    exercicio 6b

--- back-end/introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodeJS-runtime-assincrono/simpsons.js
+++ back-end/introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodeJS-runtime-assincrono/simpsons.js
@@ -1,11 +1,26 @@
 const fs = require('fs').promises;
+const readline = require('readline-sync');
 
 async function lerTodo() {
   const dados = await fs.readFile('./simpsons.json', 'utf-8');
   const simpsons = JSON.parse(dados);
-  simpsons.forEach(simp => {
+  simpsons.forEach((simp) => {
     console.log(`${simp.id} - ${simp.name}`);
   });
 }
 
+async function dadosPersonagem(id) {
+  const dados = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(dados);
+  const personagem = simpsons.find((simp) => id === Number(simp.id));
+  if(!personagem)
+    throw new Error('id não encontrado');
+
+  console.log(personagem);
+}
+
+
+
 lerTodo();
+const personId = readline.question('Qual o id está buscando? ');
+dadosPersonagem(Number(personId));
